6fe540192df889da78ba0d4f0f0193e8c69d59a8
municorn <municorn@musicaloft.com>
2024-12-04 23:36:08 -0700

    fix(mpris): fix mpris tile

--- widget/mpris.tsx
+++ widget/mpris.tsx
@@ -1,38 +1,49 @@
 import { makeTile } from "./utils";
 import { Variable, bind } from "astal";
 import Mpris from "gi://AstalMpris";
+import AstalMpris from "gi://AstalMpris?version=0.1";
 
 const mpris = Mpris.get_default();
 
 const MPRIS_PLAYING_ICON = "\u{F0F74}";
 const MPRIS_PAUSED_ICON = "\u{F03E4}";
 
-export function Media() {
-  const fallback = {
-    icon: MPRIS_PAUSED_ICON,
-    primary: "No players found",
-    secondary: "",
-    visible: false,
-  };
+export function Media(): JSX.Element {
+  return (
+    <>
+      {bind(mpris, "players").as((players) => {
+        let player =
+          players.find(
+            (p) => p.playback_status === AstalMpris.PlaybackStatus.PLAYING,
+          ) || players[0];
 
-  const tile = bind(mpris, "players").as((players) => {
-    if (players[0]) {
-      const { playback_status, artist, title } = players[0];
-      return {
-        icon:
-          playback_status === Mpris.PlaybackStatus.PLAYING
-            ? MPRIS_PLAYING_ICON
-            : MPRIS_PAUSED_ICON,
-        primary: title || `Media is ${statusToString(playback_status)}`,
-        secondary: artist,
-        visible: playback_status !== Mpris.PlaybackStatus.STOPPED,
-      };
-    } else {
-      return fallback;
-    }
-  });
+        if (player) {
+          const icon = bind(player, "playback_status").as((s) =>
+            s === AstalMpris.PlaybackStatus.PLAYING
+              ? MPRIS_PLAYING_ICON
+              : MPRIS_PAUSED_ICON,
+          );
+          const title = bind(player, "title").as(
+            (t) => t || `Media is ${statusToString(player.playback_status)}`,
+          );
+          const artist = bind(player, "artist").as((a) => a || "");
+          let visible = bind(player, "playback_status").as(
+            (s) => s !== AstalMpris.PlaybackStatus.STOPPED,
+          );
 
-  return makeTile(tile);
+          return (
+            <box spacing={12} visible={visible}>
+              <label label={icon} className={"icon"} widthRequest={16} />
+              <label label={title} className={"primary"} />
+              <label label={artist} className={"secondary"} />
+            </box>
+          );
+        } else {
+          return <></>;
+        }
+      })}
+    </>
+  );
 }
 
 function statusToString(status: Mpris.PlaybackStatus) {
