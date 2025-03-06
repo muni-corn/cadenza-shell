3c81db995244255854868b752f294e5d3d55683c
municorn <municorn@musicaloft.com>
2025-03-06 10:56:03 -0700

    fix(tray): update module with updated libraries

--- widget/tray.tsx
+++ widget/tray.tsx
@@ -1,4 +1,3 @@
-import { App, Gtk, Gdk, Astal } from "astal/gtk3";
 import { bind } from "astal";
 import Tray from "gi://AstalTray";
 
@@ -8,38 +7,16 @@ export function SysTray() {
   return (
     <box>
       {bind(tray, "items").as((items) =>
-        items.map((item) => {
-          if (item.iconThemePath) App.add_icons(item.iconThemePath);
-
-          const menu = item.create_menu();
-
-          const showMenu = (self: Gtk.Widget) => {
-            menu?.popup_at_widget(
-              self,
-              Gdk.Gravity.SOUTH,
-              Gdk.Gravity.NORTH,
-              null,
-            );
-          };
-
-          return (
-            <button
-              tooltipMarkup={bind(item, "tooltipMarkup")}
-              onDestroy={() => menu?.destroy()}
-              onClickRelease={(self, event) => {
-                if (event.button == Astal.MouseButton.PRIMARY) {
-                  item.isMenu
-                    ? showMenu(self)
-                    : item.activate(event.x, event.y);
-                } else if (event.button == Astal.MouseButton.SECONDARY) {
-                  showMenu(self);
-                }
-              }}
-            >
-              <icon gIcon={bind(item, "gicon")} />
-            </button>
-          );
-        }),
+        items.map((item) => (
+          <menubutton
+            tooltipMarkup={bind(item, "tooltipMarkup")}
+            usePopover={false}
+            actionGroup={bind(item, "actionGroup").as((ag) => ["dbusmenu", ag])}
+            menuModel={bind(item, "menuModel")}
+          >
+            <icon gicon={bind(item, "gicon")} />
+          </menubutton>
+        )),
       )}
     </box>
   );
