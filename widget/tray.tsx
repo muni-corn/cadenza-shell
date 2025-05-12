36183d0d24f4bc6cd38aae487172de0ba1c6e20c
municorn <municorn@musicaloft.com>
2025-05-12 00:10:29 -0600

    feat(tray): replace with button to expand

--- widget/tray.tsx
+++ widget/tray.tsx
@@ -1,27 +1,40 @@
 import Tray from "gi://AstalTray";
-import { bind } from "astal";
+import { bind, Variable } from "astal";
 import { Gtk } from "astal/gtk4";
 
 export function SysTray() {
   const tray = Tray.get_default();
 
+  const expanded = Variable(false);
+
   return (
-    <box>
-      {bind(tray, "items").as((items) =>
-        items.map((item) => {
-          const popover = Gtk.PopoverMenu.new_from_model(item.menu_model);
-          popover.insert_action_group("dbusmenu", item.actionGroup);
-          return (
-            <menubutton
-              tooltip-text={bind(item, "tooltipMarkup")}
-              popover={popover}
-              halign={Gtk.Align.END}
-            >
-              <image gicon={bind(item, "gicon")} />
-            </menubutton>
-          );
-        }),
-      )}
+    <box spacing={0}>
+      <revealer
+        revealChild={expanded()}
+        transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+      >
+        <box>
+          {bind(tray, "items").as((items) =>
+            items.map((item) => {
+              const popover = Gtk.PopoverMenu.new_from_model(item.menu_model);
+              popover.insert_action_group("dbusmenu", item.actionGroup);
+              return (
+                <menubutton
+                  tooltip-text={bind(item, "tooltipMarkup")}
+                  popover={popover}
+                  halign={Gtk.Align.END}
+                >
+                  <image gicon={bind(item, "gicon")} />
+                </menubutton>
+              );
+            }),
+          )}
+        </box>
+      </revealer>
+      <button
+        onClicked={() => expanded.set(!expanded.get())}
+        iconName={expanded((e) => (e ? "arrow-right" : "arrow-left"))}
+      />
     </box>
   );
 }
