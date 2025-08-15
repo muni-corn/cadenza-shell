b3a38176a89bda153835b3ee26bacf2da5b0cf02
municorn <municorn@musicaloft.com>
2025-08-14 18:46:46 -0700

    refactor: rename widget directory to widgets

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import app from "ags/gtk4/app";
 import style from "./style.scss";
-import { Bar } from "./widget/bar";
-import { NotificationCenter } from "./widget/notifications/notification-center";
-import { Notifications } from "./widget/notifications/notifications";
+import { Bar } from "./widgets/bar";
+import { NotificationCenter } from "./widgets/notifications/notification-center";
+import { Notifications } from "./widgets/notifications/notifications";
 
 app.start({
   css: style,
