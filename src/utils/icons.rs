071a15daa26f51cd820c7e1ca90841a9b0f926b0
municorn <municorn@musicaloft.com>
2025-08-17 23:01:00 -0700

    fix(icons): fix brightness icons

--- src/utils/icons.rs
+++ src/utils/icons.rs
@@ -1,11 +1,19 @@
 // Icon constants for various widgets
-pub const BRIGHTNESS_ICONS: &[&str] = &["󰃞", "󰃟", "󰃠", "󰃡", "󰃢", "󰃣"];
+pub const BRIGHTNESS_ICONS: &[&str] = &[
+    "\u{F00DB}",
+    "\u{F00DC}",
+    "\u{F00DD}",
+    "\u{F00DE}",
+    "\u{F00DF}",
+    "\u{F00E0}",
+];
 pub const VOLUME_ICONS: &[&str] = &["󰕿", "󰖀", "󰕾"];
 pub const MUTE_ICON: &str = "󰖁";
 pub const BATTERY_ICONS: &[&str] = &["󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"];
 pub const BATTERY_CHARGING_ICONS: &[&str] = &["󰢟", "󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂊", "󰂋"];
 pub const NETWORK_WIFI_ICONS: &[&str] = &["󰤯", "󰤟", "󰤢", "󰤥", "󰤨"];
 pub const NETWORK_WIRED_ICONS: &[&str] = &["󰈀"];
+
 pub const BLUETOOTH_ICONS: &[&str] = &["󰂯", "󰂱"];
 
 /// Get an icon from a list based on a percentage value
