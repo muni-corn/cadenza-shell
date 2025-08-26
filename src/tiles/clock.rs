16177fdf6051d35ef4b94c5a734118350f375846
municorn <municorn@musicaloft.com>
2025-08-25 21:55:44 -0700

    refactor(clock): use Tile component to simply clock tile implementation

--- src/tiles/clock.rs
+++ src/tiles/clock.rs
@@ -6,31 +6,26 @@ use relm4::prelude::*;
 use tokio::time::interval;
 
 use crate::icon_names;
-use crate::messages::TileOutput;
+use crate::widgets::tile::{Attention, Tile, TileInit, TileMsg, TileOutput};
 
 #[derive(Debug)]
 pub struct ClockTile {
     time: DateTime<Local>,
+    tile: Controller<Tile>,
 }
 
 #[derive(Debug)]
 pub enum ClockMsg {
-    Click,
-    UpdateTime,
-}
-
-#[derive(Debug)]
-pub struct ClockWidgets {
-    time_label: gtk::Label,
-    date_label: gtk::Label,
+    Clicked,
+    Nothing,
 }
 
 impl SimpleComponent for ClockTile {
     type Init = ();
     type Input = ClockMsg;
     type Output = TileOutput;
-    type Root = gtk::Button;
-    type Widgets = ClockWidgets;
+    type Root = gtk::Box;
+    type Widgets = ();
 
     fn init(
         _: Self::Init,
@@ -39,76 +34,60 @@ impl SimpleComponent for ClockTile {
     ) -> ComponentParts<Self> {
         let current_time = Local::now();
 
-        let model = ClockTile { time: current_time };
+        // initialize the Tile component
+        let tile = Tile::builder()
+            .launch(TileInit {
+                name: "clock".to_string(),
+                icon_name: Some(icon_names::CLOCK_ALT.to_string()),
+                primary: Some(format_time(&current_time)),
+                secondary: Some(format_date(&current_time)),
+                visible: true,
+                attention: Attention::Normal,
+                extra_classes: vec!["clock".to_string()],
+            })
+            .forward(sender.input_sender(), |output| match output {
+                TileOutput::Clicked => ClockMsg::Clicked,
+                _ => ClockMsg::Nothing,
+            });
 
-        let hbox = &gtk::Box::new(gtk4::Orientation::Horizontal, 8);
+        root.append(tile.widget());
 
-        let icon = gtk::Image::builder()
-            .icon_name(icon_names::CLOCK_ALT)
-            .build();
-
-        let time_label = gtk::Label::builder()
-            .css_classes(["clock-time"])
-            .label(model.get_time_text())
-            .build();
-
-        let date_label = gtk::Label::builder()
-            .css_classes(["clock-date"])
-            .label(model.get_date_text())
-            .build();
-
-        hbox.append(&icon);
-        hbox.append(&time_label);
-        hbox.append(&date_label);
-        root.set_child(Some(hbox));
-
-        let sender_clone = sender.clone();
-        root.connect_clicked(move |_| {
-            sender_clone.input(ClockMsg::Click);
-        });
-
-        // Update from service every second via property notify
-        let sender_clone = sender.clone();
+        // start time updates
+        let tile_sender_clone = tile.sender().clone();
         tokio::spawn(async move {
             let mut interval = interval(Duration::from_secs(1));
             loop {
                 interval.tick().await;
-                sender_clone.input(ClockMsg::UpdateTime);
+                let now = Local::now();
+                tile_sender_clone.emit(TileMsg::UpdateData {
+                    icon: Some(icon_names::CLOCK_ALT.to_string()),
+                    primary: Some(format_time(&now)),
+                    secondary: Some(format_date(&now)),
+                });
             }
         });
 
-        let widgets = ClockWidgets {
-            date_label,
-            time_label,
+        let model = ClockTile {
+            time: current_time,
+            tile,
         };
-        ComponentParts { model, widgets }
+
+        ComponentParts { model, widgets: () }
     }
 
-    fn update(&mut self, msg: Self::Input, sender: ComponentSender<Self>) {
-        match msg {
-            ClockMsg::Click => {
-                sender.output(TileOutput::Clicked("clock".to_string())).ok();
-            }
-            ClockMsg::UpdateTime => self.time = Local::now(),
-        }
-    }
-
-    fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
-        widgets.time_label.set_label(&self.get_time_text());
-        widgets.date_label.set_label(&self.get_date_text());
+    fn update(&mut self, _msg: Self::Input, _sender: ComponentSender<Self>) {
+        // TODO: handle tile clicks
     }
 
     fn init_root() -> Self::Root {
-        gtk::Button::builder().css_classes(["tile"]).build()
+        gtk::Box::new(gtk::Orientation::Horizontal, 0)
     }
 }
 
-impl ClockTile {
-    fn get_time_text(&self) -> String {
-        self.time.format("%-I:%M %P").to_string()
-    }
+fn format_time(time: &DateTime<Local>) -> String {
+    time.format("%-I:%M %P").to_string()
+}
 
-    fn get_date_text(&self) -> String {
-        self.time.format("%a, %b %-d").to_string()
-    }
+fn format_date(time: &DateTime<Local>) -> String {
+    time.format("%a, %b %-d").to_string()
 }
