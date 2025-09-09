7a744e33177d344c07bc5056e7a55d013e21c82f
municorn <municorn@musicaloft.com>
2025-09-08 22:05:36 -0600

    feat: add Service trait

--- src/services.rs
+++ src/services.rs
@@ -4,3 +4,14 @@ pub mod brightness;
 pub mod hyprland;
 pub mod network;
 pub mod notifications;
+
+pub trait Service {
+    /// The struct that represents the current state of this service.
+    type State;
+
+    /// Creates an instance of this service, spawning a thread for its logic.
+    fn launch() -> Self;
+
+    /// Adds a callback to this service that will be called upon updates.
+    fn with(self, callback: impl FnMut(Self::State) + Send + Sync + 'static) -> Self;
+}
