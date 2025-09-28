431fd60d05a2f7d0ce6f12226e5424e33b48f320
municorn <municorn@musicaloft.com>
2025-09-28 06:26:23 -0600

    fix(notifications): implement actions and action execution correctly

--- src/notifications/types.rs
+++ src/notifications/types.rs
@@ -1,8 +1,21 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use zbus::zvariant::Type;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+fn de_actions<'de, D>(deserializer: D) -> Result<Vec<(String, String)>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    // actions are represented as a list of strings. even-indexed strings are
+    // identifiers; odd-indexed strings are the user-facing labels presented on
+    // action buttons
+    let v = Vec::<String>::deserialize(deserializer)?;
+    let evens = v.iter().step_by(2).cloned();
+    let odds = v.iter().skip(1).step_by(2).cloned();
+    Ok(evens.zip(odds).collect())
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Notification {
     pub id: u32,
     pub app_name: String,
@@ -14,7 +27,9 @@ pub struct Notification {
     pub urgency: NotificationUrgency,
     pub timeout: i32,
     pub timestamp: i64,
-    pub actions: Vec<String>,
+
+    #[serde(deserialize_with = "de_actions")]
+    pub actions: Vec<(String, String)>,
 }
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Default, Debug, Type, Clone, Copy)]
