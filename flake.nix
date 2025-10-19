b970f296acb00a6981418446280a7e97988bc2f4
municorn <municorn@musicaloft.com>
2025-10-18 23:39:51 -0600

    feat: add dprint formatter for SCSS files with malva plugin configuration

--- flake.nix
+++ flake.nix
@@ -188,6 +188,33 @@
 
           # formatting
           treefmt.programs = {
+            dprint = {
+              enable = true;
+              includes = [
+                "*.scss"
+              ];
+              settings = {
+                indentWidth = 2;
+                useTabs = false;
+
+                plugins = (
+                  pkgs.dprint-plugins.getPluginList (
+                    plugins: with plugins; [
+                      g-plane-malva
+                    ]
+                  )
+                );
+
+                malva = {
+                  hexColorLength = "short";
+                  quotes = "preferSingle";
+                  formatComments = true;
+                  declarationOrder = "smacss";
+                  keyframeSelectorNotation = "keyword";
+                  preferSingleLine = true;
+                };
+              };
+            };
             mdformat = {
               enable = true;
               package = pkgs.mdformat.withPlugins (
