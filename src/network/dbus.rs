4a160824f808461c4ca3b93a26387bc924415a55
municorn <municorn@musicaloft.com>
2025-12-23 21:32:13 -0700

    feat(network): add remaining dbus properties to AccessPoint proxy

--- src/network/dbus.rs
+++ src/network/dbus.rs
@@ -75,11 +75,54 @@ pub trait WiredDevice {
     default_service = "org.freedesktop.NetworkManager"
 )]
 pub trait AccessPoint {
+    /// Flags describing the capabilities of the access point.
+    #[zbus(property)]
+    fn flags(&self) -> zbus::Result<u32>;
+
+    /// Flags describing the access point's capabilities according to WPA
+    /// (Wifi Protected Access).
+    #[zbus(property)]
+    fn wpa_flags(&self) -> zbus::Result<u32>;
+
+    /// Flags describing the access point's capabilities according to the RSN
+    /// (Robust Secure Network) protocol.
+    #[zbus(property)]
+    fn rsn_flags(&self) -> zbus::Result<u32>;
+
+    /// The Service Set Identifier identifying the access point.
     #[zbus(property)]
     fn ssid(&self) -> zbus::Result<Vec<u8>>;
 
+    /// The radio channel frequency in use by the access point, in MHz.
+    #[zbus(property)]
+    fn frequency(&self) -> zbus::Result<u32>;
+
+    /// The hardware address (BSSID) of the access point.
+    #[zbus(property)]
+    fn hw_address(&self) -> zbus::Result<String>;
+
+    /// Describes the operating mode of the access point.
+    #[zbus(property)]
+    fn mode(&self) -> zbus::Result<u32>;
+
+    /// The maximum bitrate this access point is capable of, in
+    /// kilobits/second (Kb/s).
+    #[zbus(property)]
+    fn max_bitrate(&self) -> zbus::Result<u32>;
+
+    /// The bandwidth announced by the access point in MHz.
+    #[zbus(property)]
+    fn bandwidth(&self) -> zbus::Result<u32>;
+
+    /// The current signal quality of the access point, in percent.
     #[zbus(property)]
     fn strength(&self) -> zbus::Result<u8>;
+
+    /// The timestamp (in CLOCK_BOOTTIME seconds) for the last time the access
+    /// point was found in scan results. A value of -1 means the access point
+    /// has never been found in scan results.
+    #[zbus(property)]
+    fn last_seen(&self) -> zbus::Result<i32>;
 }
 
 #[proxy(
